52d10905fe8be63e7dca234e0a013b1aa0e1d5c6
Sachin Sahani <115858954+Sachinmca778@users.noreply.github.com>
2026-03-04 00:15:43 +0530

    Time: 4 ms (99.53%), Space: 47.8 MB (69.16%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int i = 0;
+        int j= n-1;
+        while(i<j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return checkPalindrone(s,i+1,j) || checkPalindrone(s,i,j-1);
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+
+    public boolean checkPalindrone(String s,int i, int j) {
+        while(i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+               return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
