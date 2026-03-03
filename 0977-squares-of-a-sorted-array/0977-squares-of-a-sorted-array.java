253be088b9ff3ce93dd14c6df7d36acb4f8c12c2
Sachin Sahani <115858954+Sachinmca778@users.noreply.github.com>
2026-03-03 22:19:35 +0530

    Time: 1 ms (99.87%), Space: 47.2 MB (75.84%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,14 +1,40 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] arr = new int[nums.length];
-        int square =0;
-        for(int i=0; i<arr.length ; i++) {
-             arr[i]  = nums[i]*nums[i];
-             
+        int i = 0;
+        int j = nums.length-1;
+        int k = nums.length-1;
+
+        while(i<=j) {
+            if(Math.abs(nums[i]) < Math.abs(nums[j])) {
+                arr[k] = nums[j]*nums[j];
+                j--;
+                k--;
+            } else {
+                arr[k] = nums[i]*nums[i];
+                i++;
+                k--;
+            }
         }
-        Arrays.sort(arr);
 
         return arr;
+
+        
         
     }
+
+    //Brute Force
+    // public int[] sortedSquares(int[] nums) {
+    //     int[] arr = new int[nums.length];
+    //     int square =0;
+    //     for (int i=0; i<arr.length ; i++) {
+    //          arr[i]  = nums[i]*nums[i];
+             
+    //     }
+        
+    //     Arrays.sort(arr);
+
+    //     return arr;
+        
+    // }
 }
