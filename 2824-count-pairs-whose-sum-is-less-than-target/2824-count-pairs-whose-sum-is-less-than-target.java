ee85c79d7dfa6be2dbb0018eb17a0afda4deb9e3
Sachin Sahani <115858954+Sachinmca778@users.noreply.github.com>
2026-05-12 01:43:21 +0530

    Time: 3 ms (47.58%), Space: 43.5 MB (98.46%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,14 +1,38 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int n = nums.size();
-        int count =0;
-        for(int i=0; i<n-1; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(nums.get(i)+ nums.get(j) < target) {
-                    count++;
-                }
+
+        //Optimal Approach (Two Pointer)
+        Collections.sort(nums);
+         int n = nums.size();
+         int left =0;
+         int right = n-1;
+         int count =0;
+
+         while (left <= right) {
+            int sum = nums.get(left) + nums.get(right);
+            if(sum < target) {
+               count += (right - left);
+
+               left++;
+            } else {
+                right--;
             }
-        }
-        return count;
+         }
+
+         return count;
+
+
+        //Brute force Approach
+
+        // int n = nums.size();
+        // int count =0;
+        // for(int i=0; i<n-1; i++) {
+        //     for(int j=i+1; j<n; j++) {
+        //         if(nums.get(i)+ nums.get(j) < target) {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 }
