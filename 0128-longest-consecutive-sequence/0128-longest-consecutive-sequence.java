6d52d7107b60f5225b5af69d984b3271048a1764
Sachin Sahani <115858954+Sachinmca778@users.noreply.github.com>
2026-09-26 18:39:16 +0530

    Time: 30 ms (73.85%), Space: 95.6 MB (52.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,34 +1,63 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
 
-        if (nums.length == 0) {
-            return 0;
+        // if (nums.length == 0) {
+        //     return 0;
+        // }
+
+        // int count = 1;
+        // int consecutive_count = 1;
+
+        // Arrays.sort(nums);
+
+        // for (int i = 0; i < nums.length - 1; i++) {
+
+        //     // Consecutive number
+        //     if (nums[i] + 1 == nums[i + 1]) {
+        //         count++;
+        //         consecutive_count = Math.max(consecutive_count, count);
+        //     }
+
+        //     // Duplicate number -> ignore
+        //     else if (nums[i] == nums[i + 1]) {
+        //         continue;
+        //     }
+
+        //     // Not consecutive
+        //     else {
+        //         count = 1;
+        //     }
+        // }
+
+        // return consecutive_count;
+
+
+
+       HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
         }
 
-        int count = 1;
-        int consecutive_count = 1;
+        int longest = 0;
 
-        Arrays.sort(nums);
+        for (int num : set) {
 
-        for (int i = 0; i < nums.length - 1; i++) {
+            // Only start from sequence beginning
+            if (!set.contains(num - 1)) {
 
-            // Consecutive number
-            if (nums[i] + 1 == nums[i + 1]) {
-                count++;
-                consecutive_count = Math.max(consecutive_count, count);
-            }
+                int current = num;
+                int count = 1;
 
-            // Duplicate number -> ignore
-            else if (nums[i] == nums[i + 1]) {
-                continue;
-            }
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
 
-            // Not consecutive
-            else {
-                count = 1;
+                longest = Math.max(longest, count);
             }
         }
 
-        return consecutive_count;
+        return longest;
     }
 }
